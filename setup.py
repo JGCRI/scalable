dcd5d941b1ab90a14362c5a0c6f4090737259e69
sash19 <shashank.laamba@gmail.com>
2024-08-24 09:30:35 -0700

    Upgraded metadata like pyproject and setup

--- setup.py
+++ setup.py
@@ -1,40 +1,18 @@
 #!/usr/bin/env python
 
-from os.path import exists
-
-from versioneer import get_version, get_cmdclass
 from setuptools import setup
-
-with open("requirements.txt") as f:
-    install_requires = f.read().strip().split("\n")
-
-extras_require = {}
-
-extras_require["test"] = [
-    "pytest",
-    "pytest-asyncio",
-    "cryptography",
-]
-
-if exists("README.rst"):
-    with open("README.rst") as f:
-        long_description = f.read()
-else:
-    long_description = ""
+from . import versioneer
 
 setup(
     name="scalable",
-    version=get_version(),
-    cmdclass=get_cmdclass(),
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
     description="Assist with running models on job queing systems like Slurm",
     url="https://www.pnnl.gov",
     python_requires=">=3.8",
     license="BSD-2-Clause",
     packages=["scalable"],
     include_package_data=True,
-    install_requires=install_requires,
     tests_require=["pytest >= 2.7.1"],
-    extras_require=extras_require,
-    long_description=long_description,
     zip_safe=False,
 )
